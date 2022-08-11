95b660bc2fcf7d3ace9e6f6f89a58589cd013814
i18fn <fn.nk.2002@gmail.com>
2022-08-11 14:20:27 +0900

    Add something

--- template.c
+++ template.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <stdarg.h>
+#include <string.h>
 
 #define ARRAY_SIZE(a) ( sizeof(a) / sizeof(a[0]) )
 
@@ -15,12 +17,61 @@
 #define REP(n) for (int i = 0; i < (int)(n); i++)
 #define BIT(n) ( 1LL << (n) )
 
-#define STOI(str) ((int)strtol(str, NULL, 10))
+#define STOI(str) ( (int)strtol(str, NULL, 10) )
+#define STOD(str) ( strtod(str, NULL) )
+
+#define STR_EQUAL(str1, str2) ( !strcmp(str1, str2) )
+
+#define MAX2(a, b) (a > b ? a : b)
+#define MIN2(a, b) (a < b ? a : b)
+
+int scan_int() {
+    char str[100];
+    scanf("%s", str);
+    return STOI(str);
+}
+
+double scan_double() {
+    char str[100];
+    scanf("%s", str);
+    return STOD(str);
+}
+
+int max(int len, ...) {
+    va_list va_ptr;
+    va_start(va_ptr, len);
+
+    int max_value = va_arg(va_ptr, int);
+
+    for (int i = 1; i < len; i++) {
+        int num = va_arg(va_ptr, int);
+        max_value = MAX2(num, max_value);
+    }
+
+    va_end(va_ptr);
+    return max_value;
+}
+
+int min(int len, ...) {
+    va_list va_ptr;
+    va_start(va_ptr, len);
+
+    int min_value = va_arg(va_ptr, int);
+
+    for (int i = 1; i < len; i++) {
+        int num = va_arg(va_ptr, int);
+        min_value = MIN2(num, min_value);
+    }
+
+    va_end(va_ptr);
+    return min_value;
+}
+
 
 int main(void) {
-    char str[100];
-
-    scanf("%s", str);
+    int a = scan_int();
+    
+    double b = scan_double();
 
     printf("%d\n", );
 
